d13659d3ff8d56222deea4ce5a804f9045d639d0
Marsevil <marsevil@hotmail.fr>
2020-11-13 22:14:15 +0100

    doc(ConfigReader): Document class.
    --
    
    configFile

--- sources/ConfigReader.hpp
+++ sources/ConfigReader.hpp
@@ -16,22 +16,48 @@
 
 namespace sf = std::filesystem;
 
+/**
+ * Represent memory projection of configuration.
+ */
 struct Config {
     sf::path UNCRYPTED_FOLDER;
     sf::path ENCRYPTED_FOLDER;
     std::string PASSWORD;
 };
 
+/**
+ * This class read a config file to fill the Config structure.
+ */
 class ConfigReader {
+    /**
+     * Constant path to the $HOME/encrypt-me_config.env.
+     */
     static const sf::path CONFIG_FILE;
+    /**
+     * Current configuration.
+     */
     static std::unique_ptr<Config> config;
-    
+
+    /**
+     * Suppress all space char in the string.
+     * @param str can be empty.
+     */
     static void formatString(std::string& str);
+    /**
+     * Change all char to lower case.
+     * @param str
+     */
     static void lowerString(std::string& str);
 
 public:
+    /**
+     * Read the config file to fill config.
+     */
     static void readConfig();
 
+    /**
+     * @return can be not relevant if call before readConfig function.
+     */
     inline static std::unique_ptr<Config> const& getConfig() { return config; }
 };
 
